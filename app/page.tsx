04e30f5407e11b8fc1914985442d75ab7608e903
Rafael Calderon <recalderon93@gmail.com>
2025-03-31 11:46:07 -0400

    feat: Add landing page Banners [PAN-18]

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import GreatSolutionsBanner from "../components/banners/GreatSolutionBanner";
+import KnowMoreBanner from "../components/banners/KnowMoreBanner";
+import UnlockPotentialBanner from "../components/banners/UnlockPotentialBanner";
 import HeroSection from "../components/Hero/Hero";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <GreatSolutionsBanner />
+      <KnowMoreBanner />
+      <UnlockPotentialBanner />
     </>
   );
 }
